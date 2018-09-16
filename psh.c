7af4f63bbe78c7cc21143a58bf7aa6289f4a0a13
Pratyush Yadav <me@yadavpratyush.com>
2018-09-16 16:11:59 +0530

    Added the ability to execute simple commands without any arguments
    We fork() into a child, and then exec the program needed. Rather simple 
    for now. There is no way to pass command-line arguments yet.

--- psh.c
+++ psh.c
@@ -1,14 +1,59 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <stdbool.h>
+#include <unistd.h>
+#include <errno.h>
+#include <err.h>
+#include <string.h>
+
+#include <sys/wait.h>
+#include <sys/types.h>
 
 #include <psh.h>
 
 static void
 shell_loop()
 {
-	while (1) {
+	int sz, i;
+	pid_t pid;
+	char cmd_buf[ARG_MAX], *progname;
 
+	while (true) {
+		printf("> ");
+
+		/* Read from stdin. Then fork and exec the new command. */
+		fgets(cmd_buf, ARG_MAX, stdin);
+
+		/* Parse the string and get the command line arguments to pass. */
+
+		/* Get the length of the program's name. */
+		for (sz = 0; cmd_buf[sz] != ' ' && cmd_buf[sz] != '\n'; sz++);
+
+		progname = malloc(sizeof(*progname) * sz);
+		if (progname == NULL) {
+			err(ENOMEM, "Failed to allocate buffer for program name");
+		}
+
+		for (i = 0; i < sz; i++) {
+			progname[i] = cmd_buf[i];
+		}
+		/* Append the NULL terminator. */
+		progname[i] = 0;
+
+		pid = fork();
+		if (pid < 0) {
+			err(errno, "Fork failed");
+		}
+
+		if (pid == 0) {
+			execlp(progname, progname, NULL);
+			DPRINTF("execlp returned %d\n", errno);
+			err(errno, "Failed to exec the program");
+		} else {
+			wait(NULL);
+			free(progname);
+			/* Now read another command. */
+		}
 	}
 }
 
